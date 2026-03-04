36632a893fa805cd8375f1e3ed26eed157f9eee6
inYrDim <managerdims@gmail.com>
2026-03-05 02:06:03 +0800

    refactor: Move database utility scripts `check-db.js` and `fix-constraint.js` into a new `scripts/` directory.

---
+++ db/migrations/1772647543567-AddNewRoles.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {import('typeorm').MigrationInterface} MigrationInterface
+ * @typedef {import('typeorm').QueryRunner} QueryRunner
+ */
+
+/**
+ * @class
+ * @implements {MigrationInterface}
+ */
+module.exports = class AddNewRoles1772647543567 {
+
+    /**
+     * @param {QueryRunner} queryRunner
+     */
+    async up(queryRunner) {
+    }
+
+    /**
+     * @param {QueryRunner} queryRunner
+     */
+    async down(queryRunner) {
+    }
+
+}
